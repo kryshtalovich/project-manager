c323df7dbcfe9ae14a60094f7ff122b7364077be
kryshtalovich <kryshtalovich@gmail.com>
2020-12-05 01:59:56 +0300

    Added create users from admin panel

--- manager/src/Controller/UsersController.php
+++ manager/src/Controller/UsersController.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Model\User\Entity\User\User;
+use App\Model\User\UseCase\Create;
+use App\Model\User\UseCase\Edit;
 use App\ReadModel\User\UserFetcher;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +19,13 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class UsersController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("", name="users")
      * @param Request $request
@@ -27,6 +37,64 @@ class UsersController extends AbstractController
         $users = $fetcher->all();
 
         return $this->render('app/users/index.html.twig', compact('users'));
+    }
+
+    /**
+     * @Route("/create", name="users.create")
+     * @param Request $request
+     * @param Create\Handler $handler
+     * @return Response
+     */
+    public function create(Request $request, Create\Handler $handler): Response
+    {
+        $command = new Create\Command();
+
+        $form = $this->createForm(Create\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                return $this->redirectToRoute('users');
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/users/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="users.edit")
+     * @param User $user
+     * @param Request $request
+     * @param Edit\Handler $handler
+     * @return Response
+     */
+    public function edit(User $user, Request $request, Edit\Handler $handler): Response
+    {
+        $command = Edit\Command::fromUser($user);
+
+        $form = $this->createForm(Edit\Form::class, $command);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                return $this->redirectToRoute('users.show', ['id' => $user->getId()]);
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -11,18 +11,17 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\HasLifecycleCallbacks
  * @ORM\Table(name="user_users", uniqueConstraints={
- *      @ORM\UniqueConstraint(columns={"email"}),
- *      @ORM\UniqueConstraint(columns={"reset_token_token"})
+ *     @ORM\UniqueConstraint(columns={"email"}),
+ *     @ORM\UniqueConstraint(columns={"reset_token_token"})
  * })
  */
 class User
 {
-    public const STATUS_WAIT = 'wait';
+    private const STATUS_WAIT = 'wait';
     public const STATUS_ACTIVE = 'active';
-    public const STATUS_NEW= 'new';
+    private const STATUS_BLOCKED = 'blocked';
 
     /**
-     * @var Id
      * @ORM\Column(type="user_user_id")
      * @ORM\Id
      */
@@ -33,23 +32,18 @@ class User
      */
     private $date;
     /**
-     * @var Email
+     * @var Email|null
      * @ORM\Column(type="user_user_email", nullable=true)
      */
     private $email;
     /**
-     * @var string
-     * @ORM\Column(type="string", nullable=true, name="password_hash")
+     * @var string|null
+     * @ORM\Column(type="string", name="password_hash", nullable=true)
      */
     private $passwordHash;
     /**
-     * @var ResetToken|null
-     * @ORM\Embedded(class="ResetToken", columnPrefix="reset_token_")
-     */
-    private $resetToken;
-    /**
      * @var string|null
-     * @ORM\Column(type="string", nullable=true, name="confirm_token")
+     * @ORM\Column(type="string", name="confirm_token", nullable=true)
      */
     private $confirmToken;
     /**
@@ -68,17 +62,22 @@ class User
      */
     private $newEmailToken;
     /**
+     * @var ResetToken|null
+     * @ORM\Embedded(class="ResetToken", columnPrefix="reset_token_")
+     */
+    private $resetToken;
+    /**
      * @var string
      * @ORM\Column(type="string", length=16)
      */
     private $status;
     /**
      * @var Role
-     * @ORM\Column(type="user_user_role")
+     * @ORM\Column(type="user_user_role", length=16)
      */
     private $role;
     /**
-     * @var Network[]\ArrayCollection
+     * @var Network[]|ArrayCollection
      * @ORM\OneToMany(targetEntity="Network", mappedBy="user", orphanRemoval=true, cascade={"persist"})
      */
     private $networks;
@@ -92,12 +91,21 @@ class User
         $this->networks = new ArrayCollection();
     }
 
-    public static function signUpByEmail(Id $id, \DateTimeImmutable $date, Name $name, Email $email, string $hash, string $confirmToken): User
+    public static function create(Id $id, \DateTimeImmutable $date, Name $name, Email $email, string $hash): self
     {
         $user = new self($id, $date, $name);
         $user->email = $email;
         $user->passwordHash = $hash;
-        $user->confirmToken = $confirmToken;
+        $user->status = self::STATUS_ACTIVE;
+        return $user;
+    }
+
+    public static function signUpByEmail(Id $id, \DateTimeImmutable $date, Name $name, Email $email, string $hash, string $token): self
+    {
+        $user = new self($id, $date, $name);
+        $user->email = $email;
+        $user->passwordHash = $hash;
+        $user->confirmToken = $token;
         $user->status = self::STATUS_WAIT;
         return $user;
     }
@@ -105,7 +113,7 @@ class User
     public function confirmSignUp(): void
     {
         if (!$this->isWait()) {
-            throw new \DomainException('User is already confirmed!');
+            throw new \DomainException('User is already confirmed.');
         }
 
         $this->status = self::STATUS_ACTIVE;
@@ -122,13 +130,12 @@ class User
 
     public function attachNetwork(string $network, string $identity): void
     {
-        foreach ($this->networks as $existing){
-            if ($existing->isForNetwork($network)){
+        foreach ($this->networks as $existing) {
+            if ($existing->isForNetwork($network)) {
                 throw new \DomainException('Network is already attached.');
             }
         }
         $this->networks->add(new Network($this, $network, $identity));
-        $this->status = self::STATUS_ACTIVE;
     }
 
     public function detachNetwork(string $network, string $identity): void
@@ -136,39 +143,49 @@ class User
         foreach ($this->networks as $existing) {
             if ($existing->isFor($network, $identity)) {
                 if (!$this->email && $this->networks->count() === 1) {
-                    throw new \DomainException('Unable to detach last identity.');
+                    throw new \DomainException('Unable to detach the last identity.');
                 }
                 $this->networks->removeElement($existing);
                 return;
             }
         }
-        throw new \DomainException('Networks is not attached.');
+        throw new \DomainException('Network is not attached.');
     }
 
     public function requestPasswordReset(ResetToken $token, \DateTimeImmutable $date): void
     {
-        if (!$this->isActive()){
-            throw new \DomainException('User is not active!');
+        if (!$this->isActive()) {
+            throw new \DomainException('User is not active.');
         }
-        if (!$this->email){
-            throw new \DomainException('Email is not specified!');
+        if (!$this->email) {
+            throw new \DomainException('Email is not specified.');
         }
-        if ($this->resetToken && !$this->resetToken->isExpiredTo($date)){
-            throw new \DomainException('Resetting is already requested!');
+        if ($this->resetToken && !$this->resetToken->isExpiredTo($date)) {
+            throw new \DomainException('Resetting is already requested.');
         }
         $this->resetToken = $token;
+    }
+
+    public function passwordReset(\DateTimeImmutable $date, string $hash): void
+    {
+        if (!$this->resetToken) {
+            throw new \DomainException('Resetting is not requested.');
+        }
+        if ($this->resetToken->isExpiredTo($date)) {
+            throw new \DomainException('Reset token is expired.');
+        }
+        $this->passwordHash = $hash;
+        $this->resetToken = null;
     }
 
     public function requestEmailChanging(Email $email, string $token): void
     {
         if (!$this->isActive()) {
-            throw new \DomainException('User is not active!');
+            throw new \DomainException('User is not active.');
         }
-
         if ($this->email && $this->email->isEqual($email)) {
-            throw new \DomainException('Email is already same!');
+            throw new \DomainException('Email is already same.');
         }
-
         $this->newEmail = $email;
         $this->newEmailToken = $token;
     }
@@ -181,22 +198,9 @@ class User
         if ($this->newEmailToken !== $token) {
             throw new \DomainException('Incorrect changing token.');
         }
-
         $this->email = $this->newEmail;
         $this->newEmail = null;
         $this->newEmailToken = null;
-    }
-
-    public function passwordReset(\DateTimeImmutable $date, string $hash): void
-    {
-        if (!$this->resetToken){
-            throw new \DomainException('Resetting is not requested.');
-        }
-        if ($this->resetToken->isExpiredTo($date)){
-            throw new \DomainException('Reset token is expired.');
-        }
-        $this->passwordHash = $hash;
-        $this->resetToken = null;
     }
 
     public function changeName(Name $name): void
@@ -204,16 +208,34 @@ class User
         $this->name = $name;
     }
 
+    public function edit(Email $email, Name $name): void
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
     public function changeRole(Role $role): void
     {
-        if ($this->role->isEqual($role)){
-            throw new \DomainException('Role is already same!');
+        if ($this->role->isEqual($role)) {
+            throw new \DomainException('Role is already same.');
         }
         $this->role = $role;
     }
-    public function isNew(): bool
+
+    public function activate(): void
     {
-        return $this->status === self::STATUS_NEW;
+        if ($this->isActive()) {
+            throw new \DomainException('User is already active.');
+        }
+        $this->status = self::STATUS_ACTIVE;
+    }
+
+    public function block(): void
+    {
+        if ($this->isBlocked()) {
+            throw new \DomainException('User is already blocked.');
+        }
+        $this->status = self::STATUS_BLOCKED;
     }
 
     public function isWait(): bool
@@ -226,6 +248,11 @@ class User
         return $this->status === self::STATUS_ACTIVE;
     }
 
+    public function isBlocked(): bool
+    {
+        return $this->status === self::STATUS_BLOCKED;
+    }
+
     public function getId(): Id
     {
         return $this->id;
@@ -234,11 +261,6 @@ class User
     public function getDate(): \DateTimeImmutable
     {
         return $this->date;
-    }
-
-    public function getName(): Name
-    {
-        return $this->name;
     }
 
     public function getEmail(): ?Email
@@ -256,12 +278,19 @@ class User
         return $this->confirmToken;
     }
 
-    /**
-     * @return Network[]
-     */
-    public function getNetworks(): array
+    public function getName(): Name
     {
-        return $this->networks->toArray();
+        return $this->name;
+    }
+
+    public function getNewEmail(): ?Email
+    {
+        return $this->newEmail;
+    }
+
+    public function getNewEmailToken(): ?string
+    {
+        return $this->newEmailToken;
     }
 
     public function getResetToken(): ?ResetToken
@@ -279,14 +308,12 @@ class User
         return $this->status;
     }
 
-    public function getNewEmail(): ?Email
+    /**
+     * @return Network[]
+     */
+    public function getNetworks(): array
     {
-        return $this->newEmail;
-    }
-
-    public function getNewEmailToken(): ?string
-    {
-        return $this->newEmailToken;
+        return $this->networks->toArray();
     }
 
     /**
@@ -294,7 +321,7 @@ class User
      */
     public function checkEmbeds(): void
     {
-        if ($this->resetToken->isEmpty()){
+        if ($this->resetToken->isEmpty()) {
             $this->resetToken = null;
         }
     }
